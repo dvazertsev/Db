f22b5c7087633d01073f6c7e04c15a31a06343b3
dvazertsev <70569957+dvazertsev@users.noreply.github.com>
2020-12-01 19:03:38 +0200

    Add files via upload

--- controler.py
+++ controler.py
@@ -4,18 +4,18 @@ from model import *
 from view import *
 
 
-def database(request):
-	command_view()
-
-	if request=='random':
-		db_random()
-	elif request=='search':
-		db_search()
-	elif request=='delete':
-		db_delete()
-	elif request=='insert':
-		db_add()
-	elif request=='update':
-		db_update()
+def database():
+	request = command_view()
 	
+	if request =='random':
+		db_random()
+	elif request =='search':
+		db_search()
+	elif request =='delete':
+		db_delete()
+	elif request =='insert':
+		db_add()
+	elif request =='update':
+		db_update()
+	 
 database()
--- view.py
+++ view.py
@@ -2,5 +2,4 @@ def command_view():
 	print('+ =+=+=+=+=+=+=+=___Program___+=+=___commands___+=+=+=+=+=+=+=+=+ +')
 	print('+   random   +   insert   +   update   +   delete   +   search   + ')
 	print('+ =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+= +')
-	
-request=input('Enter command > ')
+	return input('Enter command > ')
